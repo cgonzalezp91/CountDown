60f6a963ce162b4094a1cae875432f7e4b1ec6e8
Carlos <cgonzalezp91@gmail.com>
2018-04-04 22:51:14 -0500

    Countdown finished

---
+++ README.md
@@ -0,0 +1,14 @@
+## A Simple CountDown Made in JS.
+
+## Getting Started
+
+This is a Simple CountDown App with styles in Flexbox
+
+## License
+
+This project is licensed under the MIT License - see the LICENSE file for details
+
+## Authors
+
+Carlos Gonzalez - <cgonzalezp91@gmail.com> - <https://twitter.com/cgonzalezp91> - <https://www.linkedin.com/in/cgonzalezp91><br />
+
---
+++ index.js
@@ -0,0 +1,33 @@
+// Date limit of our countdown
+let countDate = new Date('Apr 06 2018 00:00:00')
+//Arrow Fuction to bring us the Remaining Time, Seconds, Minutes, Hours and Days
+const TimeLeft = countDate => {
+    let now = new Date(),
+    rTime = (new Date(countDate) - now + 1000) / 1000,
+    rSeconds = ( '0' + Math.floor(rTime % 60)).slice(-2),
+    rMinutes = ( '0' + Math.floor(rTime / 60 % 60)).slice(-2),
+    rHours = ( '0' + Math.floor(rTime / 3600 % 24)).slice(-2),
+    rDays = Math.floor(rTime / (3600 * 24))
+    // Return an object with all the date data
+    return {
+        rTime,
+        rSeconds,
+        rMinutes,
+        rHours,
+        rDays
+    }
+};
+//Set up our interval to start the countdown
+ const countdown = setInterval(() => {
+    //call the arrow function sending the string with the CountDown Date
+    let t = TimeLeft(countDate)
+    //Set to all divs the current data
+    document.getElementById("days").textContent = t.rDays + '\r\n'
+    document.getElementById("hours").textContent = t.rHours + '\r\n'
+    document.getElementById("minutes").textContent = t.rMinutes + '\r\n'
+    document.getElementById("seconds").textContent = t.rSeconds + '\r\n'
+    if (t.rTime <= 1) {
+        clearInterval(countdown)
+        alert('The time is over!')
+    }
+ }, 1000);
